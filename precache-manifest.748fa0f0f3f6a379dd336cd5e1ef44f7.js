817fb941db1447cf958a6cf5d60659069c7585b1
Faiz Hameed <faizftb@gmail.com>
2019-06-14 20:27:20 +0530

    Updates

---
+++ precache-manifest.748fa0f0f3f6a379dd336cd5e1ef44f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113f380832edf64ed95a6c43a1691df3",
+    "url": "/chessBoardModel/index.html"
+  },
+  {
+    "revision": "5cdba9e82351c4a9bdf0",
+    "url": "/chessBoardModel/static/css/main.9610fed5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/chessBoardModel/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "5cdba9e82351c4a9bdf0",
+    "url": "/chessBoardModel/static/js/main.fed9c88a.chunk.js"
+  },
+  {
+    "revision": "e699d6faf866008a1cb9",
+    "url": "/chessBoardModel/static/js/runtime~main.ccbf3e2b.js"
+  }
+]);
